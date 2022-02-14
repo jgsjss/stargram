1264d384a7afdf44a4693bf02ca43a75088f99be
jgsjss9854 <87510168+jgsjss@users.noreply.github.com>
2022-02-14 18:44:37 +0900

    Add files via upload

---
+++ precache-manifest.b09dae2a084d8d929e5e1ac988387a55.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "280775cf92dcd74035ee",
+    "url": "/stargram/css/app.a732f923.css"
+  },
+  {
+    "revision": "750877617f0adef65afbb8f67cd4813e",
+    "url": "/stargram/follower.json"
+  },
+  {
+    "revision": "8fe6607848524a987dcdbf4fc68e0145",
+    "url": "/stargram/index.html"
+  },
+  {
+    "revision": "280775cf92dcd74035ee",
+    "url": "/stargram/js/app.85adcede.js"
+  },
+  {
+    "revision": "51d0b94c1816e6b956cc",
+    "url": "/stargram/js/chunk-vendors.ff63980c.js"
+  },
+  {
+    "revision": "7a48e533dbca2832e097778c0f48b6a6",
+    "url": "/stargram/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/stargram/robots.txt"
+  }
+]);
